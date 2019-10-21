637c4445d4034d694b058135d7f6845f52062daf
Conner <wangyuli@credit91.com>
2019-10-21 17:41:00 +0800

    feat:更新库

--- YLKit/UIKit/Additions/UITableView+YLKit.m
+++ YLKit/UIKit/Additions/UITableView+YLKit.m
@@ -110,7 +110,6 @@
 }
 
 - (void)addSectionCornerRadiusWithCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath cornerRadius:(CGFloat)cornerRadius leftRightMargin:(CGFloat)leftRightMargin topBottomMargin:(CGFloat)topBottomMargin {
-    
     // 设置cell的背景色为透明，如果不设置这个的话，则原来的背景色不会被覆盖
     cell.backgroundColor = UIColor.clearColor;
     // 创建一个shapeLayer
